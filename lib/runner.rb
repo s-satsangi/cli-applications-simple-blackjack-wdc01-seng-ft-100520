a0429ee52a65def1e8d2a1762722ef5a13c4e4c9
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-28 17:37:16 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,14 +1,5 @@
 require_relative "blackjack.rb"
 require 'pry'
 #binding.pry
-welcome
-card_total = initial_round
-#binding.pry
-until (card_total > 21 ) do
-  card_total += hit? card_total
-  display_card_total card_total
-  #binding.pry
-end
 
-end_game card_total 
 runner
