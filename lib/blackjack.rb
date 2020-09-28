85d4ba53b5e66defdef7169276b96c662b2a5703
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-28 17:37:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -68,5 +68,15 @@ end
 
 def runner
   # code runner here
+  welcome
+card_total = initial_round
+#binding.pry
+until (card_total > 21 ) do
+  card_total += hit? card_total
+  display_card_total card_total
+  #binding.pry
+end
+
+end_game card_total 
 end
     
